c18652791e0c449d7e921ae09157c0f104e90c0b
grosucristi <grosuca229@gmail.com>
2022-03-13 11:30:08 +0200

    gallery changes (transaprent bg, 0 margin)

--- scripts/photoswipe_init.js
+++ scripts/photoswipe_init.js
@@ -138,6 +138,7 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
 
       // define options (if needed)
       options = {
+          bgOpacity: 0.8,
 
           // define gallery index (for URL)
           galleryUID: galleryElement.getAttribute('data-pswp-uid'),
